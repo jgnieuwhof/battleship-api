3e6ec5ede14813930bd7dda2806ace3d682e3c68
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-12-15 01:54:49 -0500

    these aren't configurable anyway

--- src/sockets/newGame.js
+++ src/sockets/newGame.js
@@ -2,18 +2,13 @@ import uuid from 'uuid/v4';
 
 import { gameStates } from '../common/constants';
 
-const newGame = ({ io, db, user, actions }) => (
-  { numberOfShips, shotsPerTurn, dimensions },
-  fn
-) => {
+const newGame = ({ io, db, user, actions }) => ({ dimensions }, fn) => {
   const gameId = uuid();
   const game = {
     id: gameId,
     state: gameStates.matchmaking,
     host: user.id,
     hostName: db.get('users', user.id).name,
-    numberOfShips,
-    shotsPerTurn,
     dimensions,
     events: [],
     boards: {}
